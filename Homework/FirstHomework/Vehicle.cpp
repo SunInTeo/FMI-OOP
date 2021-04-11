f219a5b596c5323fc4e135c784734bc6d7eb3779
SunInTeo <72868219+SunInTeo@users.noreply.github.com>
2021-04-11 23:52:55 +0300

    Added Vehicle

---
+++ Homework/FirstHomework/Vehicle.cpp
@@ -0,0 +1,24 @@
+#include "Vehicle.h"
+
+Vehicle::Vehicle(const char *registration, const char *description, std::size_t space)
+{
+    MyString tempRegistration(registration);
+    MyString tempDescription(description);
+
+    licenseNumber = tempRegistration;
+    descriptionAboutVehicle = tempDescription;
+
+    numberOfParkingSpaces = space;
+}
+const char *Vehicle::registration() const
+{
+    return licenseNumber.c_str();
+}
+const char *Vehicle::description() const
+{
+    return descriptionAboutVehicle.c_str();
+}
+std::size_t Vehicle::space() const
+{
+    return numberOfParkingSpaces;
+}
---
+++ Homework/FirstHomework/Vehicle.h
@@ -0,0 +1,19 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include "MyString.h"
+
+class Vehicle
+{
+    MyString licenseNumber;
+    MyString descriptionAboutVehicle;
+    std::size_t numberOfParkingSpaces = 0;
+
+public:
+    Vehicle(const char *registration, const char *description, std::size_t space);
+    const char* registration() const;
+    const char* description() const;
+    std::size_t space() const;
+};
+
+#endif
