b470fdfec1751e254d5ac4b6a74845567d416f32
SunInTeo <72868219+SunInTeo@users.noreply.github.com>
2021-04-16 14:16:26 +0300

    Added Garage

--- Homework/FirstHomework/Header files/Garage.h
+++ Homework/FirstHomework/Header files/Garage.h
@@ -3,49 +3,29 @@
 
 #include "Vehicle.h"
 
-
-/*
--Garage(std::size_t size) -- създава гараж с максимално място за паркиране size.
-Гарантирайте, че обектите могат да се копират и унищожават коректно. Ако е нужно, 
-реализирайте експлицитно всички функции от rule of 3 (бонус: гарантирайте, че работят коректно всички от rule of 5). 
-Ако това не е нужно, следвайте rule of 0.
--void insert(Vehicle& v) -- добавя превозното средство v в гаража. Ако операцията не успее 
-(например няма достатъчно памет, в гаража няма повече място за паркиране, вече има кола със същия регистрационен номер), 
-да се хвърля изключение. Операцията да дава strong exception guarantee.
--void erase(const char* registration) -- премахва колата с регистрационен номер registration от гаража.
-Ако такава няма, да не се прави нищо. При премахването на кола от гаража е допустимо да се промени редът на останалите в гаража. 
-Това условие ще ви позволи при премахване на елемент да поставите последния елемент от масива на мястото на премахнатия, 
-вместо да правите left shift.
--const Vehicle& at(std::size_t pos) const -- достъп до елемента намиращ се на позиция pos. 
-Ако такъв няма, да се хвърля изключение std::out_of_range.
--const Vehicle& operator[](std::size_t pos) const -- достъп до елемента намиращ се на позиция pos. 
-Функцията да не прави проверка за коректност дали pos е валидна позиция. (В debug режим assert-вайте дали pos е валидна позиция).
--bool empty() const -- Проверява дали гаражът е празен.
--std::size_t size() const -- брой елементи (превозни средства) в гаража.
--void clear() -- изчиства съдържанието на гаража. Това означава, 
-че в него не се съдържа нито една кола. Капацитетът му обаче остава непроменен. 
-Така в него могат отново да се добавят нови коли.
--const Vehicle* find(const char* registration) const -- намира и връща превозното средство с регистрационен номер registration в гаража. 
-Ако такова няма, да се върне nullptr.*/
 class Garage
 {
-    Vehicle **vehicleArray = nullptr;
-    std::size_t _capacity;
-    std::size_t _sizeTaken;
-    std::size_t _numberOfVehicles;
+    Vehicle **vehicleArray;
+    std::size_t capacity;
+    std::size_t sizeTaken;
+    std::size_t numberOfVehicles;
 
-    void copy(const Garage &other);
-    void deallocate();
-    void resize();
 public:
     Garage() = delete;
     Garage(std::size_t size);
-    Garage(const Garage &other);
     ~Garage();
-    
-    void insert(Vehicle& v);
 
-    Garage &operator=(const Garage &other);
+    void insert(Vehicle &v);
+    void erase(const char *registration);
+
+    const Vehicle &at(std::size_t pos) const;
+    const Vehicle &operator[](std::size_t pos) const;
+
+    bool empty() const;
+    std::size_t size() const;
+    void clear();
+
+    const Vehicle *find(const char *registration) const;
 };
 
 #endif
--- Homework/FirstHomework/Source files/Garage.cpp
+++ Homework/FirstHomework/Source files/Garage.cpp
@@ -1,88 +1,99 @@
-#include <iostream>
 #include "..\Header files\Garage.h"
 
 using std::cout;
 using std::endl;
 using std::size_t;
 
-void Garage::copy(const Garage &other)
-{
-    Vehicle **buffer = new Vehicle *[other._capacity];
-
-    for (size_t i = 0; i < other._numberOfVehicles; ++i)
-    {
-        buffer[i] = new Vehicle(*(other.vehicleArray[i]));
-    }
-
-    for (size_t i = 0; i < other._numberOfVehicles; ++i)
-    {
-        delete[] vehicleArray[i];
-    }
-    delete[] vehicleArray;
-
-    vehicleArray = buffer;
-    _numberOfVehicles = other._numberOfVehicles;
-    _capacity = other._capacity;
-    _sizeTaken = other._sizeTaken;
-}
-void Garage::deallocate()
-{
-}
-void Garage::resize()
-{
-}
-
 Garage::Garage(size_t size)
 {
-    _numberOfVehicles = 0;
-    _sizeTaken = 0;
-    _capacity = size;
-    vehicleArray = new Vehicle *[_capacity];
+    numberOfVehicles = 0;
+    sizeTaken = 0;
+    capacity = size;
+    vehicleArray = new Vehicle *[capacity];
 }
-Garage::Garage(const Garage &other)
-{
-    copy(other);
-}
+
 Garage::~Garage()
 {
-    for (std::size_t i = 0; i < _numberOfVehicles; ++i)
-    {
-        delete vehicleArray[i];
-    }
-
     delete[] vehicleArray;
 }
-Garage &Garage::operator=(const Garage &other)
-{
-    if (this != &other)
-    {
-        copy(other);
-    }
 
-    return *this;
-}
-void Garage::insert(Vehicle& v)
+void Garage::insert(Vehicle &v)
 {
-    if(v.space() + _sizeTaken > _capacity)
+    if (v.space() + sizeTaken > capacity)
     {
         throw std::out_of_range("There is not enough space for this vehicle");
     }
 
-    bool doesExistInGarage = false;
-    for(size_t i = 0; i < _numberOfVehicles; ++i)
+    for (size_t i = 0; i < numberOfVehicles; ++i)
     {
-        if(!m_strcmp(vehicleArray[i]->registration(), v.registration()))
+        if (!m_strcmp(vehicleArray[i]->registration(), v.registration()))
         {
-            doesExistInGarage = true;
+            throw std::invalid_argument("Vehicle already exists in the garage");
         }
     }
 
-    if(doesExistInGarage)
+    vehicleArray[numberOfVehicles++] = &v;
+    sizeTaken += v.space();
+}
+
+void Garage::erase(const char *registration)
+{
+    std::size_t index = capacity;
+    for (std::size_t i = 0; i < numberOfVehicles; ++i)
     {
-        throw std::invalid_argument("Vehicle already exists in the garage");
+        if (!m_strcmp(vehicleArray[i]->registration(), registration))
+        {
+            index = i;
+        }
     }
 
-    Vehicle *vhcl = new Vehicle(v.registration(), v.description(), v.space());
-    vehicleArray[_numberOfVehicles++] = vhcl;
-    _sizeTaken += v.space();
+    if (index != capacity)
+    {
+        vehicleArray[index] = vehicleArray[capacity - 1];
+        sizeTaken -= vehicleArray[index]->space();
+        --numberOfVehicles;
+    }
+}
+
+const Vehicle &Garage::at(std::size_t pos) const
+{
+    if (pos >= sizeTaken)
+    {
+        throw std::out_of_range("Invalid position");
+    }
+    return *vehicleArray[pos];
+}
+
+const Vehicle &Garage::operator[](std::size_t pos) const
+{
+    assert(pos <= sizeTaken);
+    return *vehicleArray[pos];
+}
+
+bool Garage::empty() const
+{
+    return !numberOfVehicles;
+}
+std::size_t Garage::size() const
+{
+    return numberOfVehicles;
+}
+void Garage::clear()
+{
+    for (std::size_t i = 0; i < numberOfVehicles; ++i)
+    {
+        vehicleArray[i] = nullptr;
+    }
+    numberOfVehicles = 0;
+}
+
+const Vehicle *Garage::find(const char *registration) const
+{
+    for (std::size_t i = 0; i < numberOfVehicles; ++i)
+    {
+        if (!m_strcmp(vehicleArray[i]->registration(), registration))
+        {
+            return vehicleArray[i];
+        }
+    }
 }
