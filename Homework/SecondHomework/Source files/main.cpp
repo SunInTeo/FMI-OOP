6b26aa5613ce826811830f10e346a8654ec8974c
SunInTeo <72868219+SunInTeo@users.noreply.github.com>
2021-05-18 22:53:31 +0300

    Still having problems with the commands

---
+++ Homework/SecondHomework/Source files/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include "../Header files/CommandAllocator.h"
+
+int main(int argc, char *argv[])
+{
+    /*for (int i = 0; i < argc; i++)
+    {
+        std::cout << argv[i] << '\n';
+    }*/
+    if (argc == 1)
+    {
+        CommandAllocator myCmdAlloc;
+        myCmdAlloc.start();
+    }
+    else if (argc == 2)
+    {
+        std::cout << "Opening file...\n";
+        std::string tempStr(argv[1]);
+        CommandAllocator myCmdAlloc(tempStr);
+        myCmdAlloc.start();
+    }
+    else
+    {
+
+        std::cerr << "Too many arguments!\n";
+        return 1;
+    }
+
+    return 0;
+}
